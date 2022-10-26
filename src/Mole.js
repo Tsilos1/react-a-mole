3020724205304bc4371eb60cbabe9fa67328c58e
Tsilos Kosbab <38735446+Tsilos1@users.noreply.github.com>
2022-10-26 10:37:38 -0500

    worked on app.js

---
+++ src/Mole.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+
+function MoleContainer(props){
+    return (
+        <div>
+            <h2> Mole Container </h2>
+            <Mole />
+        </div>
+    )
+}
+
+function createMoleHill() {
+   
+}
+
+
+
+
+
+
+export default Mole
